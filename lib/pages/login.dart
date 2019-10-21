94eb6f1dd853deddf7ecc087f9fd745d085a9f3d
JulianH99 <juliancorredor99@gmail.com>
2019-10-21 07:53:10 -0500

    Added input text component to login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:tutorial_app/widgets/input_text.dart';
 import '../widgets/circle.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,11 +12,21 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Container(
+        body: GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
         width: size.width,
         height: size.height,
         child: Stack(
@@ -36,10 +48,12 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             SingleChildScrollView(
-              child: SafeArea(
-                child: Container(
-                  width: size.width,
+              child: Container(
+                width: size.width,
+                height: size.height,
+                child: SafeArea(
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       Column(
                         children: <Widget>[
@@ -67,7 +81,67 @@ class _LoginPageState extends State<LoginPage> {
                         ],
                       ),
                       Column(
-                        children: <Widget>[],
+                        children: <Widget>[
+                          ConstrainedBox(
+                              constraints:
+                                  BoxConstraints(maxWidth: 350, minWidth: 350),
+                              child: Form(
+                                child: Column(
+                                  children: <Widget>[
+                                    InputText(
+                                      label: "EMAIL ADDRESS",
+                                    ),
+                                    SizedBox(
+                                      height: 30,
+                                    ),
+                                    InputText(
+                                      label: "PASSWORD",
+                                    )
+                                  ],
+                                ),
+                              )),
+                          SizedBox(
+                            height: 50,
+                          ),
+                          ConstrainedBox(
+                            constraints:
+                                BoxConstraints(maxWidth: 350, minWidth: 350),
+                            child: CupertinoButton(
+                              padding: EdgeInsets.symmetric(vertical: 17),
+                              color: Colors.pinkAccent,
+                              borderRadius: BorderRadius.circular(4),
+                              onPressed: () {},
+                              child: Text(
+                                'Sign in',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "New to this app?",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.black54),
+                              ),
+                              CupertinoButton(
+                                child: Text(
+                                  'Sign up',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.pinkAccent),
+                                ),
+                                onPressed: () {},
+                              )
+                            ],
+                          ),
+                          SizedBox(height: size.height * 0.08,)
+                        ],
                       )
                     ],
                   ),
@@ -77,6 +151,6 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ),
       ),
-    );
+    ));
   }
 }
